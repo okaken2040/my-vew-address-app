3f024516288e9d49d835d52e31a7199706ac36ac
Kensei Okamoto <okamotokensei@okamotokenshounoMacBook-puro.local>
2020-02-20 15:05:26 +0900

    Cloud Firestoreへのデータの保存

--- src/store/index.js
+++ src/store/index.js
@@ -41,7 +41,8 @@ export default new Vuex.Store({
     toggleSideMenu({ commit }) {
       commit('toggleSideMenu')
     },
-    addAddress({ commit }, address) {
+    addAddress({ getters, commit }, address) {
+      if(getters.uid) firebase.firestore().collection(`users/${getters.uid}/addresses`).add(address)
       commit('addAddress',address)
     }
   },
@@ -49,6 +50,7 @@ export default new Vuex.Store({
   },
   getters: {
     userName: state => state.login_user ? state.login_user.displayName : '',
-    photoURL: state => state.login_user ? state.login_user.photoURL : ''
+    photoURL: state => state.login_user ? state.login_user.photoURL : '',
+    uid: state => state.login_user ? state.login_user.uid : null
   }
 })
